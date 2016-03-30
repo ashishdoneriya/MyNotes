3cee83fbb266241f34b7d8b652f88c08a04ab308
Ashish Doneriya <ashishdoneriya@gmail.com>
2016-03-31 03:14:04 +0530

    Added Files

---
+++ Ubuntu Files/github.sh
@@ -0,0 +1,16 @@
+cd /home/ashish/angularJumbune
+git init
+git config --global user.name "ashishdoneriya"
+git config --global user.email "ashishdoneriya@gmail.com"
+git remote add origin git@githu.com:EngineeringExcellence/Jumbune.git
+git pull origin master
+
+
+
+mkdir /home/ashish/WebHDFS
+cd /home/ashish/WebHDFS
+git init
+git config --global user.name "ashishdoneriya"
+git config --global user.email "ashishdoneriya@gmail.com"
+git remote add origin git@github.com:ashishdoneriya/WebHDFS.git
+git pull origin master
---
+++ Ubuntu Files/sources/removeApps.sh
@@ -0,0 +1,20 @@
+sudo apt-get remove amazon* 
+sudo apt-get remove aisleriot* 
+sudo apt-get remove gnome-mahjongg* 
+sudo apt-get remove deja-dup-* 
+sudo apt-get remove transmission-gtk*
+sudo apt-get remove totem*
+sudo apt-get remove activity-log-manager* 
+sudo apt-get remove unity-lens-photos* 
+sudo apt-get remove unity-scope-gdrive* 
+sudo apt-get remove brasero* 
+sudo apt-get remove landscape-client-ui-install* 
+sudo apt-get remove unity-control-center-signon* 
+sudo apt-get remove printer-driver-foo2zjs-common*
+sudo apt-get remove empathy*
+sudo apt-get remove thunderbird*
+sudo apt-get remove onboard*
+sudo apt-get remove gnome-orca*
+#Unity Dash search result :-
+sudo apt-get remove unity-scope-musicstores
+sudo apt-get autoremove
